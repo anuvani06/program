a9fcae4537196c2f739c8c8d4d16793ee1439e9e
Anusuya <anusya116@gmail.com>
2026-07-09 12:14:21 +0530

    Add files via upload

---
+++ sort_colour.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<Integer>a=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            a.add(i);
+        }
+    }
+}
